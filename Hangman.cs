b81ebb56424c0c3c958434d3a1f76ff4c6cc84d3
noahbreyes <148291831+noahbreyes@users.noreply.github.com>
2025-04-27 20:06:33 -0700

    Add files via upload
    Added PlayGame()

--- Hangman.cs
+++ Hangman.cs
@@ -4,8 +4,10 @@ Purpose: Incldues main
 */
 
 using System;
+using System.Collections.Generic;
 
 public class Hangman{
+    
     static void Main(string[] args) {
         Console.WriteLine("*************HANG MAN*************\n");
             Console.WriteLine("ENTER AN OPTION\n");
@@ -18,7 +20,11 @@ public class Hangman{
                 switch(input.ToLower()){
                     case "start":
                         //start game;
-                        while (true){
+                        bool keepPlaying = true;
+                        while (keepPlaying){
+
+                            PlayGame();
+
                             Console.WriteLine("Do you want to play one more game?\n");
                             Console.WriteLine("Enter y/n\n");
                             string newGame = Console.ReadLine() ?? "";
@@ -29,6 +35,7 @@ public class Hangman{
 
                                 case "n":
                                     Console.WriteLine("Good bye!\n");
+                                    keepPlaying = false;
                                     return;
                                 
                                 default:
@@ -37,7 +44,7 @@ public class Hangman{
 
                             }
                         }
-
+                        break;
                     case "exit":
                         Console.WriteLine("Good bye!\n");
                         return;
@@ -47,6 +54,69 @@ public class Hangman{
                         break;
                 }
                 
+        }
+    }
+
+    static void PlayGame(){
+        string[] wordList = {"computer", "soccer", "pancake", "challenge", "butterfly"};
+        Random rand = new Random();
+
+        string word = wordList[rand.Next(wordList.Length)];
+        char[] guessed = new string('_', word.Length).ToCharArray();
+        HashSet<char> triedLetters = new HashSet<char>();
+        
+        int lives = 6;
+
+        Console.WriteLine("\nNew Game Started!\n");
+
+        while (lives > 0 && new string(guessed) != word)
+        {
+            Console.WriteLine($"Word: {new string(guessed)}");
+            Console.WriteLine($"Lives remaining: {lives}");
+            Console.WriteLine("Enter your guess (one letter):");
+
+            string input = Console.ReadLine() ?? "";
+            if (input.Length != 1 || !char.IsLetter(input[0]))
+            {
+                Console.WriteLine("Please enter a single letter.\n");
+                continue;
+            }
+
+            char guess = char.ToLower(input[0]);
+
+            if (triedLetters.Contains(guess))
+            {
+                Console.WriteLine("You already tried that letter!\n");
+                continue;
+            }
+
+            triedLetters.Add(guess);
+
+            if (word.Contains(guess))
+            {
+                Console.WriteLine("Good guess!\n");
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == guess)
+                    {
+                        guessed[i] = guess;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong guess!\n");
+                lives--;
+            }
+        }
+
+        if (new string(guessed) == word)
+        {
+            Console.WriteLine($"Congratulations! You guessed the word: {word}\n");
+        }
+        else
+        {
+            Console.WriteLine($"You lost! The word was: {word}\n");
         }
     }
 }
